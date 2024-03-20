4c9664be8a594d400dc6deeec8bc2cc06742a9b0
Juniormalinga <ST10438896@Vcconnect.edu.za>
2024-03-20 11:10:40 +0200

    Jamican Humour

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("helloo world");
+		System.out.print("helloo world \t");
+		System.out.println("\n Mi Bomboclatt");
 
 	}
 
